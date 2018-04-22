697c32ab48ef69c2489a931384afda7c7c803001
Youssef <youssefaacr7@gmail.com>
2018-04-22 10:24:11 +0200

    some 3d models and some packages

---
+++ War-Of-Worms/Assets/Models/Island/Scripts/WaterScroller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScroller : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ War-Of-Worms/Assets/Scripts/SceneMov.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneMov : MonoBehaviour {
+
+	public void Menue_Moove(string sceneName)
+    {
+        
+    }
+}
