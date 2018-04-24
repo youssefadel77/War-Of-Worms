a2d7eb0c77b2a5b9256788674f50f463af1eea8a
yahia <yayaibrahim30@yahoo.com>
2018-04-24 14:40:33 +0200

    merge

---
+++ War-Of-Worms/Assets/Scripts/changePOV.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changePOV : MonoBehaviour {
+    public GameObject  thePlayer;
+    public Camera firstCam;
+    public Camera thirdCam;
+    private bool check;
+
+    // Use this for initialization
+    void Start () {
+        thirdCam.enabled = true ;
+        firstCam.enabled = false;
+        check = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (check)
+            { 
+                thirdCam.enabled = false;
+                firstCam.enabled = true;
+            }
+            else
+            {
+                thirdCam.enabled = true;
+                firstCam.enabled = false;
+            }
+            check = !check;
+        }
+        
+            
+        
+        
+
+
+
+    }
+}
